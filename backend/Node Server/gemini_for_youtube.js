c96ef1b64dc88972bd17f5e0a36def11a1a1532c
BISHAL SARANIAH <113280653+bishalsaraniah@users.noreply.github.com>
2024-09-27 22:21:04 +0530

    Create gemini_for_youtube.js

---
+++ backend/Node Server/gemini_for_youtube.js
@@ -0,0 +1,41 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => {
+    res.send("Hello Gemini");
+})
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+const generate = async(prompt) => {
+    try{
+        const result = await model.generateContent(prompt);
+        console.log(result.response.text());
+        return result.response.text();
+    } catch(err){
+        console.log(err);
+    }
+}
+
+app.get('/api/content', async(req,res) => {
+    try{
+        const data = req.body.question;
+        const result = await generate(data);
+        res.send({
+            "result": result
+        })
+    } catch {
+        res.send("error: " + err)
+    }
+})
+
+
+app.listen(3000, () =>{
+    console.log('Server is up and running in port 3000')
+})
