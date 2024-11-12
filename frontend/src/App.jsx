b2f7f1d28e454dbd8d4728d7168a2db6b6e7fddd
BISHAL SARANIAH <113280653+bishalsaraniah@users.noreply.github.com>
2024-11-12 18:44:42 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,10 +7,6 @@ import Home from './pages/Home/Home'
 // components
 import Header from './Components/Header/Header'
 import ProductInterface from './pages/app/ProductInterface'
-import Articlesummarizer from './pages/app/Subpages/Articlesummarizer'
-import Textsummarizer from './pages/app/Subpages/Textsummarizer'
-import Pdfsummarizer from './pages/app/Subpages/Pdfsummarizer'
-import VideoSummarizer from './pages/app/Subpages/VideoSummarizer'
 
 function App() {
 
@@ -23,10 +19,6 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/appinterface' element={<ProductInterface />} />
           <Route path='/appinterface/:aiproduct' element={<ProductInterface />} />
-          <Route path='/appinterface/articlesummarizer' element={<Articlesummarizer />} />
-          <Route path='/appinterface/textsummarizer' element={<Textsummarizer />} />
-          <Route path='/appinterface/pdfsummarizer' element={<Pdfsummarizer />} />
-          <Route path='/appinterface/videosummarizer' element={<VideoSummarizer />} />
         </Routes>
       </Router>
     </>
